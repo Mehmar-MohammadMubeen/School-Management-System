73ba538d52f1d62bcf886c358787831fe11746b8
Mohammad Mubeen Mehmar <ma.wafa50@outlook.com>
2023-12-10 14:09:02 +0100

    Get theAdd Teacher  button and add an event listener to handle its click

--- JS/teacher.js
+++ JS/teacher.js
@@ -2,4 +2,20 @@ document.addEventListener("DOMContentLoaded", function () {
     let emptyStorage = [];
     let data = JSON.parse(localStorage.getItem("teachers")) || emptyStorage;
     renderAllTeachers(data); 
+    let addTeacherButton = document.getElementById("addTeacherButton");
+    addTeacherButton.addEventListener("click", function () {
+      let newName = prompt("Enter the name of the new teacher:");
+      let newExpertise = prompt("Enter the expertise of the new teacher:");
+      let newDescription = prompt(
+        "Enter additional details for the new teacher:"
+      );
+      let newTeacher = {
+        name: newName,
+        expertise: newExpertise,
+        description: newDescription,
+      };
+      data.push(newTeacher);
+      localStorage.setItem("teachers", JSON.stringify(data));
+      renderAllTeachers(data);
+    });
 });
