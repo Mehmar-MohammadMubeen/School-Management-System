bd70b0c8e5d5bbffb88c54ea7c1560240482c1fe
Mohammad Mubeen Mehmar <ma.wafa50@outlook.com>
2023-12-10 14:27:51 +0100

    A Function to render an individual teacher card and Create the HTML structure for the teacher card.

--- JS/teacher.js
+++ JS/teacher.js
@@ -31,4 +31,33 @@ document.addEventListener("DOMContentLoaded", function () {
           teacherContainer.appendChild(row);
         }
       }
+      function renderTeacherCard(teacher, parentElement) {
+        let card = document.createElement("div");
+        card.classList.add("col-md-4", "mb-4");
+    
+        card.innerHTML = `
+        <div class="card text-center mb-3" style="width: 100%; height: 100%;">
+        <div class="card-header d-flex justify-content-between align-items-center">
+            <h5 class="card-title text-decoration-left">${teacher.name}</h5>
+            <div>
+              <button class="btn btn-link edit-button">
+                <i class="far fa-edit aa" style="color: blue"></i>
+              </button>
+              <button class="btn btn-link delete-button">
+                <i class="fas fa-minus-circle aa" style="color: blue"></i>
+              </button>
+            </div>
+          </div>
+          <div class="card-body">
+            <h6 class="card-subtitle mb-2 text-body-secondary">${teacher.expertise}</h6>
+            <p class="card-text">${teacher.description}</p>
+          </div>
+          <div class="card-footer">
+            <a href="./students.html" class="card-link" style="text-decoration: none; color: blue; margin-right: 5px;">Students</a>
+            <a href="./Class.html" class="card-link" style="text-decoration: none; color: blue;">Classes</a>
+          </div>
+          </div>
+          
+        `;
+      }
 });
